8b377aeb73645a0961b3ce12378d8ee6e5623e49
Nicolò <nicolo.biancalani@stud.unifi.it>
2023-08-07 09:52:41 +0200

    Account files added +Account.cpp +Account.h

---
+++ Account.cpp
@@ -0,0 +1,54 @@
+#include "Account.h"
+
+//
+// Created by Nicolò on 02/08/2023.
+bool Account::loadFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        return false;
+    }
+
+    transactions.clear(); // Rimuovi le transazioni esistenti
+
+    int typeInt;
+    double amount;
+
+    while (file >> typeInt >> amount) {
+        Transaction::Type type = static_cast<Transaction::Type>(typeInt);
+        Transaction transaction(type, amount);
+        transactions.push_back(transaction);
+    }
+
+    file.close();
+    return true;
+}
+bool Account::saveToFile(const std::string& filename) const {
+    std::ofstream file(filename);
+    if (!file.is_open()) {
+        std::cout<<"Errore nel salvataggio su file."<<std::endl;
+        return false;
+    }
+
+    for (const Transaction& transaction : transactions) {
+        file << static_cast<int>(transaction.getType()) << " " << transaction.getAmount() << "\n";
+    }
+
+    file.close();
+    return true;
+}
+double Account::calculateBalance() const {
+    double balance = 0.0;
+    for (const Transaction& transaction : transactions) {
+        if (transaction.getType() == Transaction::Type::DEPOSIT) {
+            balance += transaction.getAmount();
+        } else {
+            balance -= transaction.getAmount();
+        }
+    }
+    return balance;
+}
+
+void Account::addTransaction(const Transaction& transaction) {
+    transactions.push_back(transaction);
+}
+//
---
+++ Account.h
@@ -0,0 +1,24 @@
+//
+// Created by Nicolò on 02/08/2023.
+//
+
+#ifndef UNTITLED3_ACCOUNT_H
+#define UNTITLED3_ACCOUNT_H
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "Transaction.h"
+
+class Account {
+public:
+    void addTransaction(const Transaction& transaction) ;
+
+    double calculateBalance() const ;
+
+    bool saveToFile(const std::string& filename) const ;
+    bool loadFromFile(const std::string& filename);
+private:
+    std::vector<Transaction> transactions;
+};
+#endif //UNTITLED3_ACCOUNT_H
