58b61a9471eb5c23cc934b974c2d4258a34006ea
Nicolò <nicolo.biancalani@stud.unifi.it>
2023-08-07 16:24:21 +0200

    Main changed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include "Account.h"
+#include "Transaction.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+    Account account;
+    account.addTransaction(Transaction(Transaction::Type::DEPOSIT, 1000.0));
+    account.addTransaction(Transaction(Transaction::Type::WITHDRAW, 200.0));
+    account.addTransaction(Transaction(Transaction::Type::DEPOSIT, 500.0));
+    double balance = account.calculateBalance();
+    std::cout << "Balance: " << balance << std::endl;
+    account.saveToFile("transactions.txt");
+    account.addTransaction(Transaction(Transaction::Type::DEPOSIT, 1000.0));
+    account.saveToFile("transactions.txt");
+    account.loadFromFile("transactions.txt");
+    balance = account.calculateBalance();
+    std::cout << "Balance: " << balance << std::endl;
+    account.printTransactions();
     return 0;
 }
