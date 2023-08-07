3a706a05e9eb882b13f50cfc0811deccbc9f32dc
Nicolò <nicolo.biancalani@stud.unifi.it>
2023-08-07 15:58:39 +0200

    runAllTests.cpp updated

--- Test/runAllTests.cpp
+++ Test/runAllTests.cpp
@@ -1,6 +1,6 @@
 //
 // Created by Nicolò on 03/08/2023.
-#include "googletest-main/googletest/include/gtest/gtest.h"
+#include "gtest/gtest.h"
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
